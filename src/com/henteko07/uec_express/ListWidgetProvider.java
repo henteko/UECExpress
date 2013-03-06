1484d51f98f10abd423dfafd2f2b84d6f7fb3107
henteko <henteko07@gmail.com>
2013-03-07 02:13:48 +0900

    add button action

--- src/com/henteko07/uec_express/ListWidgetProvider.java
+++ src/com/henteko07/uec_express/ListWidgetProvider.java
@@ -24,16 +24,20 @@ import android.widget.Toast;
 public class ListWidgetProvider extends AppWidgetProvider {
 	public static final String EXTRA_ITEM = "com.henteko07.uec_express.TOAST_ACTION";
 	public static final String TOAST_ACTION = "com.henteko07.uec_express.EXTRA_ITEM";
+	public static final String ACTION_CLICK = "com.henteko07.uec_express.ACTION_CLICK";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		// ListView の子ビューがタップされたときに発行されるブロードキャストを
-		// 拾って、トーストを表示
 		if (intent.getAction().equals(TOAST_ACTION)) {
 			int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
 			Toast.makeText(context, "Touched view" + viewIndex,
 					Toast.LENGTH_SHORT).show();
-		}
+		}else if(ACTION_CLICK.equals(intent.getAction())) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            if(appWidgetId != 0) {
+                AppWidgetManager.getInstance(context).notifyAppWidgetViewDataChanged(appWidgetId, R.id.list_view);
+            }
+        }
 		super.onReceive(context, intent);
 	}
 
@@ -41,71 +45,30 @@ public class ListWidgetProvider extends AppWidgetProvider {
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		RemoteViews views = new RemoteViews(context.getPackageName(),
-				R.layout.listwidget_layout);
-
-		// 1. ListView に紐付けるRemoteViewsService（ここではListWidgetService）
-		// を指定するためのインテント
-		Intent intent = new Intent(context, ListWidgetService.class);
-		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-		intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-
-		// テスト用
-		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-				.permitAll().build());
-
-		HttpClient httpClient = new DefaultHttpClient();
-		StringBuilder uri = new StringBuilder("http://henteko07.com:6789");
-		HttpGet request = new HttpGet(uri.toString());
-		HttpResponse httpResponse = null;
-
-		try {
-			httpResponse = httpClient.execute(request);
-		} catch (Exception e) {
-			Log.d("JSONSampleActivity", "Error Execute : " + e);
-		}
-
-		int status = httpResponse.getStatusLine().getStatusCode();
-
-		if (HttpStatus.SC_OK == status) {
-			try {
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				httpResponse.getEntity().writeTo(outputStream);
-				String json_s = outputStream.toString(); // JSONデータ
-				intent.putExtra("json", json_s);
-
-				JSONObject rootObject;
-				try {
-					rootObject = new JSONObject(json_s);
-					String last_updated = rootObject.getString("updated");
-					views.setTextViewText(R.id.last_updated, last_updated);
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} catch (Exception e) {
-				Log.d("JSONSampleActivity", "Error");
-			}
-		} else {
-			Log.d("JSONSampleActivity", "Status" + status);
-		}
-
-		// 2. 1 で作ったインテントをListView のID を指定してセット
-		views.setRemoteAdapter(R.id.list_view, intent);
-		// 3. ListView のデータが空のときに表示するビュー
-		views.setEmptyView(R.id.list_view, R.id.empty_view);
-		// 4. ListView の子ビューがタップされたときに発行するブロードキャストのひな型をセット
-		Intent toastIntent = new Intent(context, ListWidgetProvider.class);
-		toastIntent.setAction(ListWidgetProvider.TOAST_ACTION);
-		PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context,
-				0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-		views.setPendingIntentTemplate(R.id.list_view, toastPendingIntent);
-		// アプリケーションウィジェットを更新
-		appWidgetManager.updateAppWidget(appWidgetIds, views);
 		
-		// サービスの起動
-        Intent button_intent = new Intent(context, ButtonWidgetService.class);
-        context.startService(button_intent);
+		for(int appWidgetId : appWidgetIds) {
+            Intent remoteViewsFactoryIntent = new Intent(context, ListWidgetService.class);
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.listwidget_layout);
+            rv.setRemoteAdapter(R.id.list_view, remoteViewsFactoryIntent);
+    		rv.setEmptyView(R.id.list_view, R.id.empty_view);
+            setOnButtonClickPendingIntent(context, rv, appWidgetId);
+            
+            appWidgetManager.updateAppWidget(appWidgetId, rv);
+        }
 	}
+	
+	private void setOnButtonClickPendingIntent(Context ctx, RemoteViews rv, int appWidgetId) {
+        Intent btnClickIntent = new Intent(ACTION_CLICK);
+        btnClickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+        PendingIntent btnClickPendingIntent = PendingIntent.getBroadcast(
+            ctx,
+            0,
+            btnClickIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        rv.setOnClickPendingIntent(R.id.update, btnClickPendingIntent);
+    }
 
 }
--- src/com/henteko07/uec_express/ListWidgetService.java
+++ src/com/henteko07/uec_express/ListWidgetService.java
@@ -1,14 +1,28 @@
 package com.henteko07.uec_express;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.StrictMode;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
@@ -23,93 +37,130 @@ public class ListWidgetService extends RemoteViewsService {
 	}
 
 	public RemoteViewsFactory onGetViewFactory(Intent intent) {
-		List<WidgetItem> data = new ArrayList<WidgetItem>();
-		
-		String json_s = intent.getStringExtra("json");
-		
-		JSONObject rootObject;
-		try {
-			rootObject = new JSONObject(json_s);
-			JSONArray eventArray = rootObject.getJSONArray("express");
-	        
-	        for (int i = 0; i < eventArray.length(); i++) {
-	            JSONObject jsonObject = eventArray.getJSONObject(i);
-	            Log.d("JSONSampleActivity", jsonObject.getString("class"));
-	            String express_s = jsonObject.getString("class") + " : " + 
-	            					jsonObject.getString("data") + " : " + 
-	            					jsonObject.getString("time") + "時限目\n" + 
-	            					jsonObject.getString("subject") + "\n" + 
-	            					jsonObject.getString("staff");
-	            data.add(new WidgetItem(express_s));
-	        }
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-		return new ListRemoteViewFactory(this.getApplicationContext(), intent,
-				data);
+		return new ListWidgetFactory();
 	}
 	
+	private class ListWidgetFactory implements RemoteViewsFactory {
 
-	class ListRemoteViewFactory implements
-			RemoteViewsService.RemoteViewsFactory {
-		private List<WidgetItem> mData = new ArrayList<WidgetItem>();
-		private Context mContext;
+        private static final String TAG = "SampleViewFactory";
 
-		public ListRemoteViewFactory(Context context, Intent intent,
-				List<WidgetItem> data) {
-			mContext = context;
-			mData = data;
-		}
+        private JSONArray jsons = new JSONArray();
+        private String lastUpdate = "";
 
-		public void onCreate() {
-		}
+        public void onCreate() {
+            Log.v(TAG, "[onCreate]");
+        }
 
-		public void onDestroy() {
-			mData.clear();
-		}
+        public void onDataSetChanged() {
+            Log.v(TAG, "[onDataSetChanged]");
 
-		public void onDataSetChanged() {
-		}
+            fetchUpdate();
+        }
 
-		public int getCount() {
-			return mData.size();
-		}
+        public void onDestroy() {
+            Log.v(TAG, "[onDestroy]");
+        }
 
-		public long getItemId(int position) {
-			return position;
-		}
+        public RemoteViews getViewAt(int position) {
+            Log.v(TAG, "[getViewAt]: " + position);
 
-		public RemoteViews getLoadingView() {
-			// 読み込み中用のView を返す
-			return null;
-		}
+            if(jsons.length() <= 0) {
+                return null;
+            }
 
-		public RemoteViews getViewAt(int position) {
-			// ListView の子ビューに表示するRemoteViews を取得
-			RemoteViews views = new RemoteViews(mContext.getPackageName(),
-					R.layout.listwidget_item);
-			// RemoteViews 内のTextView に表示する文字を指定
-			views.setTextViewText(R.id.widget_item, mData.get(position).mText);
-			// タップされたアイテムを識別するための情報を持つインテントを作成
-			Intent fillInIntent = new Intent();
-			fillInIntent.putExtra(ListWidgetProvider.EXTRA_ITEM, position);
-			// RemoteViews がタップされたときに
-			// PendingItentTemplate + 指定されたIntent の情報
-			// を持ったIntent を発行
-			views.setOnClickFillInIntent(R.id.widget_item, fillInIntent);
-			return views;
-		}
+            RemoteViews rv = null;
 
-		public int getViewTypeCount() {
-			return 1;
-		}
+            try {
+                JSONObject json = jsons.getJSONObject(position);
 
-		public boolean hasStableIds() {
-			return true;
-		}
+                if(json != null) {
+                	String express_s = json.getString("class") + " : " + 
+        					json.getString("data") + " : " + 
+        					json.getString("time") + "時限目\n" + 
+        					json.getString("subject") + "\n" + 
+        					json.getString("staff");
+                    
+                    rv = new RemoteViews(getPackageName(), R.layout.listwidget_item);
+                    rv.setTextViewText(R.id.widget_item, express_s);
+                }
+            } catch(JSONException e) {
+                e.printStackTrace();
+            }
 
-	}
+            return rv;
+        }
+
+        public long getItemId(int position) {
+            Log.v(TAG, "[getItemId]: " + position);
+
+            return position;
+        }
+
+        public int getCount() {
+            Log.v(TAG, "[getCount]");
+
+            return jsons.length();
+        }
+
+        public RemoteViews getLoadingView() {
+            Log.v(TAG, "[getLoadingView]");
+
+            return null;
+        }
+
+
+        public int getViewTypeCount() {
+            Log.v(TAG, "[getViewTypeCount]");
+
+            return 1;
+        }
+
+        public boolean hasStableIds() {
+            Log.v(TAG, "[hasStableIds]");
+
+            return true;
+        }
+
+        private void fetchUpdate() {
+        	StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+			.permitAll().build());
+
+        	HttpClient httpClient = new DefaultHttpClient();
+            HttpResponse response = null;
+			try {
+				response = httpClient.execute(
+				    new HttpGet("http://henteko07.com:6789")
+				);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+            if(response.getStatusLine().getStatusCode() != 200) {
+                return;
+            }
+
+            try {
+            	String json_s = EntityUtils.toString(response.getEntity());
+            	JSONObject json = new JSONObject(json_s);
+            	lastUpdate = json.getString("updated");
+            	jsons = new JSONArray(json.getString("express"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
+        	httpClient.getConnectionManager().shutdown();
+        }
+    }
 
 }
